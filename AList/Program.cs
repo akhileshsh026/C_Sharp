a22f1b788b6449e3e95da82ce13b78b7916955d5
DIM-X-GOLDENSER\akhil <akhileshsh026@hotmail.com>
2017-09-17 16:21:37 +0530

    ArrayList whtever you wnat just put it inside it and operate the way you want.

---
+++ AList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public static void ArrList_Show()
+        {
+            ArrayList al = new ArrayList();
+            al.Add("Akhileesh");
+            Console.WriteLine(al.Capacity);
+
+            ArrayList obj = new ArrayList() { 1, "Kumar", 324, "Akhilesh", 546, 54, 65, 67, 6576, 86786 };
+            char[] charobj = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j' };
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++");
+
+            al.AddRange(obj);
+            al.AddRange(charobj);
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static void HashTable()
+        {
+
+        }
+    }
+}
