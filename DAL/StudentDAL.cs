31d3c66be55c6ab3cc91b4d2f32885993fd3d2ab
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2017-10-05 06:26:51 +0000

    Demonstrate the 3 tier application .

---
+++ DAL/StudentDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class StudentDAL
+    {
+        public int insert(string FileName, string Lastname, int age)
+        {
+            
+            SqlConnection constring = new SqlConnection("Server=12.76.87.192;DataBase=Student;user=sa;Password=Akhilesh@123");
+
+        }
+    }
+}
